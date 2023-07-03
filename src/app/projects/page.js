c3d296db50bc98877ed019c4dc7bfc3d5c2016ad
Henrick Amaral <henrick.vaz.amaral@gmail.com>
2023-07-02 23:23:16 -0300

    updating projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,8 +2,10 @@ import Projects from "@/components/ApiProject";
 
 export default function ProjPage(){
     return(
-        <div>
-            <Projects/>
+        <section className="flex justify-center items-center py-4 ">
+        <div className="m-2 container flex flex-col md:grid md:grid-flow-col gap-10">
+            <Projects />
         </div>
+    </section>
     )
 }
